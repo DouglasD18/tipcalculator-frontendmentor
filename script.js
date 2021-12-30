4a60fb5df211c6141ab606be03c336461b44806c
Douglas Aguiar <daguiaralcantara@gmail.com>
2021-12-30 11:16:06 -0300

    Finalização da estilização da aplicação.

---
+++ script.js
@@ -0,0 +1,9 @@
+let dolar = document.getElementById('dolar');
+let custom = document.getElementById('custom');
+let person = document.getElementById('person');
+let amount = document.getElementById('amount');
+let total = document.getElementById('total');
+let reset = document.getElementById('reset');
+let percentual = document.getElementsByClassName('percentual');
+
+console.log(percentual);
